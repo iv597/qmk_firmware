9d0ba77ddf9cdccae921ea7b91cf447eb8dfd99a
Josh Klar <j@iv597.com>
2018-08-14 22:27:03 -0700

    Merge in my Iris' config so there's just one branch

--- keyboards/iris/rules.mk
+++ keyboards/iris/rules.mk
@@ -1,3 +1,8 @@
+SRC += matrix.c \
+	   i2c.c \
+	   split_util.c \
+	   serial.c
+
 # MCU name
 MCU = atmega32u4
 
@@ -52,7 +57,7 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
@@ -61,6 +66,7 @@ RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.  Do not enable this 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-SPLIT_KEYBOARD = yes
+CUSTOM_MATRIX = yes
+TAP_DANCE_ENABLE=yes
 
 DEFAULT_FOLDER = iris/rev2
