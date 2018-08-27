a9f377096c831554fbb6851e65a9c664f490f7ed
Josh Klar <j@iv597.com>
2018-08-27 01:26:41 -0700

    Remove unused layer. Move its functions to leader macros. Build emotes.h using new out-of-repo project.

--- build_josh_levinson.sh
+++ build_josh_levinson.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
-python emotes_to_unicode_seq.py > keyboards/levinson/keymaps/josh/emotes.h
+echo "===> Generating emotes.h with qmk_emote_macro_generator"
+docker run iv597/qmk_emote_macro_generator:latest > keyboards/levinson/keymaps/josh/emotes.h
+
+echo "===> Building and flashing keyboard with DFU (requires sudo)"
 sudo make levinson/rev2:josh:dfu
--- keyboards/levinson/keymaps/josh/keymap.c
+++ keyboards/levinson/keymaps/josh/keymap.c
@@ -16,6 +16,14 @@ void matrix_scan_user(void) {
         leading = false;
         leader_end();
 #include "emotes.h"
+
+        SEQ_TWO_KEYS(KC_L, KC_S) {
+            SEND_STRING(SS_LGUI(SS_TAP(X_HOME)));
+        }
+
+        SEQ_TWO_KEYS(KC_C, KC_W) {
+            SEND_STRING(SS_LGUI(SS_TAP(X_END)));
+        }
     }
 }
 
@@ -35,30 +43,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_QUOT, KC_COMM, KC_DOT, KC_P,     KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
       KC_TAB,  KC_A,    KC_O,    KC_E,   KC_U,     KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
       KC_LGUI, KC_SCLN, KC_Q,    KC_J,   KC_K,     KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_LALT,
-      KC_LCTL, KC_LEAD/*MO(4)*/,   SHGUI,   MO(2),  MO(3),    KC_LSFT, KC_SPC,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
+      KC_LCTL, KC_LEAD, SHGUI,   MO(2),  MO(3),    KC_LSFT, KC_SPC,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
   [1] = LAYOUT_ortho_4x12(
-      KC_GESC, xxxxxxx, xxxxxxx, KC_F10, KC_F11,   KC_F12,  xxxxxxx, KC_PSLS, KC_7,    KC_8,    KC_9,    KC_PCMM,
-      KC_TAB,  xxxxxxx, xxxxxxx, KC_F7,  KC_F8,    KC_F9,   xxxxxxx, KC_PAST, KC_4,    KC_5,    KC_6,    KC_EQL,
+      KC_GESC, xxxxxxx, xxxxxxx, KC_F10, KC_F11,   KC_F12,  xxxxxxx, KC_PSLS, KC_7,    KC_8,    KC_9,    KC_BSPC,
+      KC_TAB,  xxxxxxx, xxxxxxx, KC_F7,  KC_F8,    KC_F9,   xxxxxxx, KC_PAST, KC_4,    KC_5,    KC_6,    _______,
       KC_LGUI, xxxxxxx, xxxxxxx, KC_F4,  KC_F5,    KC_F6,   xxxxxxx, KC_PMNS, KC_1,    KC_2,    KC_3,    _______,
-      KC_LCTL, xxxxxxx, _______, KC_F1,  KC_F2,    KC_F3,   KC_SPC,  _______, KC_0,    KC_PDOT, xxxxxxx, KC_PPLS),
+      KC_LCTL, xxxxxxx, _______, KC_F1,  KC_F2,    KC_F3,   KC_SPC,  _______, KC_0,    KC_DOT,  xxxxxxx, KC_EQL),
 
   [2] = LAYOUT_ortho_4x12(
-      KC_GESC, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_DEL,
+      KC_GESC, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_BSLS, KC_LBRC, KC_RBRC, KC_DEL,
       KC_TAB,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_MINS,
-      KC_LGUI, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_LBRC, KC_RBRC, KC_BSLS, KC_INS,
+      KC_LGUI, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_LBRC, xxxxxxx, xxxxxxx, KC_INS,
       KC_LCTL, xxxxxxx, _______, _______, xxxxxxx, KC_LSFT, xxxxxxx, xxxxxxx, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
 
   [3] = LAYOUT_ortho_4x12(
-      KC_GESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_SLSH,
+      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_SLSH,
       KC_TAB,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_MINS,
       KC_LGUI, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
       KC_LCTL, xxxxxxx, xxxxxxx, xxxxxxx, _______, KC_LSFT, xxxxxxx, xxxxxxx, KC_MUTE, KC_VOLD, KC_VOLU, xxxxxxx),
-
-  [4] = LAYOUT_ortho_4x12(
-      LGUI(KC_HOME), xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, LGUI(KC_END),
-      xxxxxxx,       xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
-      xxxxxxx,       xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
-      xxxxxxx,       _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, RESET,   xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx)
 };
 
